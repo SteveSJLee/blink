dbe1c7647b5db47dad85acbafad70735b2f79456
Steve Lee <seung.jin.lee@mail.mcgill.ca>
2018-09-12 01:48:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from gpiozero import PWMLED
 import threading
 import random
@@ -28,17 +28,17 @@ def index():
 @app.route('/red', methods=['POST'])
 def toggleRed():
     _toggle(led_R)
-    return render_template('index.html')
+    return '', 204
 
 @app.route('/green', methods=['POST'])
 def toggleGreen():
     _toggle(led_G)
-    return render_template('index.html')
+    return '', 204
 
 @app.route('/blue', methods=['POST'])
 def toggleBlue():
     _toggle(led_B)
-    return render_template('index.html')
+    return '', 204
 
 def _toggle(led):
     flag = 0
@@ -54,6 +54,7 @@ def _set_flag():
     flag = 1
 
 def _blink_fancy():
+    global flag
     while True:
         if flag == 1:
             num = random.randrange(3)
